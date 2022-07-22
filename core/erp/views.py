46840f9118a95bf5e34461d4d6c48f5ca144bced
cotamo1901 <johncotamo@hotmail.com>
2022-07-21 22:41:35 -0500

    se actualiza base de datos

--- core/erp/views.py
+++ core/erp/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render
 
-from core.erp.models import Category
+from core.erp.models import Category, Product
 
 
 def myfirstview(request):
     data = {
-        'name': 'john',
+        'name': 'William',
         'categories': Category.objects.all()
     }
-    return render(request, 'home.html', data)
+    return render(request, 'index.html', data)
+
+
+def mysecondview(request):
+    data = {
+        'name': 'William',
+        'categories': Category.objects.all(),
+        'products': Product.objects.all()
+    }
+    return render(request, 'second.html', data)
