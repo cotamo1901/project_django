97fa4cc426e40596ed26795d28e895a75b30f726
cotamo1901 <johncotamo@hotmail.com>
2022-07-25 23:44:03 -0500

    se realiza ccambios en urls

--- project_2/urls.py
+++ project_2/urls.py
@@ -1,13 +1,13 @@
 """project_2 URL Configuration
 
-The `urlpatterns` list routes URLs to views. For more information please see:
+The `urlpatterns` list routes URLs to views_erp. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
 Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
+Function views_erp
+    1. Add an import:  from my_app import views_erp
+    2. Add a URL to urlpatterns:  path('', views_erp.home, name='home')
+Class-based views_erp
+    1. Add an import:  from other_app.views_erp import Home
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', include('core.erp.urls'))
+    path('erp/', include('core.erp.urls'))
 ]
